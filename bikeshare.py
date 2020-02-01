f96d95f61a61c4916f0168a3a278d26d225de7bf
Casey Cole <caseycole589@gmail.com>
2020-02-01 10:15:39 -0500

    feature: finished this project finally now using it for udacity git project

--- bikeshare.py
+++ bikeshare.py
@@ -54,103 +54,110 @@ def load_data(city, month, day):
     df['Start Time'] = pd.to_datetime(df['Start Time']) 
     df['month'] = df['Start Time'].dt.month
     df['day_of_week'] = df['Start Time'].dt.weekday_name
-    
+
     if month != 'all': 
         df = df.query('month == 1'.format(month))
    
     if day != 'all':
         df = df.query('day_of_week == "{}"'.format(day.title()))
-    print(df.head())
     return df
 
 
-def time_stats(df):
+def time_stats(df, city, month, day):
     """Displays statistics on the most frequent times of travel."""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
 
     # TO DO: display the most common month
-
+    if month == "all":
+        month_list = ['all', 'january', 'february', 'march', 'april', 'may', 'june', 'july',
+          'august', 'september', 'october', 'november', 'december']
+        most_common_month_index = df['month'].mode()[0]
+        month_name = month_list[most_common_month_index]
+        print("The month {} had the most travel is {}\n".format(city, month_name.title()))
 
     # TO DO: display the most common day of week
-
-
+    if day == "all":
+        print("The day {} had the most travel is {}\n".format(city, df['day_of_week'].mode()[0]))
     # TO DO: display the most common start hour
-
+    df['hour'] = df['Start Time'].dt.hour
+    print("The hour {} has the most travel was {}".format(city, df['hour'].mode()[0]))
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
 
-def station_stats(df):
+def station_stats(df, city):
     """Displays statistics on the most popular stations and trip."""
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
 
     # TO DO: display most commonly used start station
-
+    print("The most common used start station for {} was {}\n".format(city, df['Start Station'].mode()[0]))    
 
     # TO DO: display most commonly used end station
-
+    print("The most common used end station for {} was {}\n".format(city, df['End Station'].mode()[0]))    
 
     # TO DO: display most frequent combination of start station and end station trip
-
+    n_combos = df.groupby(['Start Station', 'End Station']).size().nlargest(1)
+    print('The most frequent combination of  in {} was \n{}\n'.format(city,n_combos))
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
 
-def trip_duration_stats(df):
+def trip_duration_stats(df, city):
     """Displays statistics on the total and average trip duration."""
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
 
     # TO DO: display total travel time
-
+    print("The total travel time for {} was {} minutes".format(city,df['Trip Duration'].sum() / 60 ))
 
     # TO DO: display mean travel time
-
+    print('The average travel time for {} was {} minutes'.format(city, df['Trip Duration'].mean()/60))
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
 
-def user_stats(df):
+def user_stats(df, city):
     """Displays statistics on bikeshare users."""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
 
     # TO DO: Display counts of user types
-
-
-    # TO DO: Display counts of gender
-
-
-    # TO DO: Display earliest, most recent, and most common year of birth
-
+    usertypes = df.groupby('User Type')['User Type'].count()
+    print('The count of each type of user in {} is \n{}\n'.format(city, usertypes))
+    if 'Gender' in df.columns:
+        # TO DO: Display counts of gender
+        gendertypes = df.groupby('Gender')['Gender'].count()
+        print('The count of each gender in {} are \n{}\n'.format(city, gendertypes))
+    if 'Birth Year' in df.columns:
+        # TO DO: Display earliest, most recent, and most common year of birth
+        print('In {} \nthe most common year of birth was {}\nthe most recent year of birth was {}\nthe most recent year of birth was {}\n'
+            .format(city, df['Birth Year'].mode()[0], df['Birth Year'].max(), df['Birth Year'].min()))
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
 
 def main():
-#     while True:
-    # city, month, day = get_filters()
-    # df = load_data(city, month, day)
-    df = load_data('chicago', 1, 'monday')
+    while True:
+        city, month, day = get_filters()
+        df = load_data(city, month, day)
+        time_stats(df,city, month, day)
+        station_stats(df, city)
+        trip_duration_stats(df, city)
+        user_stats(df, city)
 
-#         time_stats(df)
-#         station_stats(df)
-#         trip_duration_stats(df)
-#         user_stats(df)
-
-#         restart = input('\nWould you like to restart? Enter yes or no.\n')
-#         if restart.lower() != 'yes':
-#             break
+        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        if restart.lower() != 'yes':
+            break
           
 
 
